cabe8862692c0cf84375412e4086c23add83bb95
Prem <prem7736k@gmail.com>
2025-07-19 11:16:31 +0530

    chat container

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -3,7 +3,7 @@ import assets from '../assets/assets'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
   return (
-    <div>
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex item-center gap-2'>Martin Johnson
