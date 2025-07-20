5fcdbef03ec15966adf32b13da90abcdf35acb8a
Prem <prem7736k@gmail.com>
2025-07-20 07:35:25 +0530

    profile page update

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -32,8 +32,9 @@ const ProfilePage = () => {
            type="text" required placeholder='Your name ' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' />
            <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
            placeholder='Write profile bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
+           <button type='submit' className='bg-gradient-to-r form-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
-        <img src="" alt="" />
+        <img className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' src={assets.logo_icon} alt="" />
       </div>
     </div> 
   )
