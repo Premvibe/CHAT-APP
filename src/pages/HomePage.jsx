b7104b059f9637735229b6a6ae84357e31d6f4c5
Prem <prem7736k@gmail.com>
2025-07-21 23:01:16 +0530

    page update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,16 @@ const HomePage = () => {
 
   return (
     <div className="w-full h-screen sm:px-[15%] sm:py-[5%]">
-      
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid relative 
+        grid-cols-1 
+        ${selectedUser 
+          ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' 
+          : 'md:grid-cols-2'
+        }`}>
+        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+      </div>
     </div>
   )
 }
